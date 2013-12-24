2f992a26b88f06afcec552d9eac32f8c42a41172
Chris Wheatley <chriswheatley87@gmail.com>
2013-12-24 07:02:43 +0000

    amending tracking info

--- src/very.js
+++ src/very.js
@@ -5,5 +5,5 @@
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-46687167-1', 'wowsuch.com');
+ga('create', 'UA-46687167-1', 'wowsuch.io');
 ga('send', 'pageview');
