4398efd22617892d481db07f1ad356137fd7e9dc
Chris Wheatley <chriswheatley87@gmail.com>
2013-12-24 23:10:08 +0000

    simplifying build process for time being

--- gruntfile.js
+++ gruntfile.js
@@ -9,18 +9,8 @@ module.exports = function(grunt) {
       clean: {
         command: 'rm -rf build'
       },
-      image: {
-        command: 'cp -r src/img build/img'
-      }
-    },
-
-    inline: {
-      html: {
-        options: {
-          uglify: true
-        },
-        src: ['src/index.html'],
-        dest: ['build/']
+      build: {
+        command: 'cp -r src build'
       }
     },
 
@@ -33,11 +23,10 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.registerTask('build', ['shell:clean', 'inline', 'shell:image']);
-  grunt.registerTask('deploy', ['shell:clean', 'inline', 'shell:image', 'gh-pages']);
+  grunt.registerTask('build', ['shell:clean', 'shell:build']);
+  grunt.registerTask('deploy', ['shell:clean', 'shell:build', 'gh-pages']);
 
   grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-inline');
   grunt.loadNpmTasks('grunt-gh-pages');
 
 };
