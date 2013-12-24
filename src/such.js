cce3c4a0ced5cc62b41aac76439bb59686488362
Chris Wheatley <chriswheatley87@gmail.com>
2013-12-24 07:38:25 +0000

    make angular code minifiable

--- src/such.js
+++ src/such.js
@@ -2,7 +2,7 @@
 
 angular.module('wowSuch', []);
 
-angular.module('wowSuch').controller('dogeController', function($scope, dogeData) {
+angular.module('wowSuch').controller('dogeController', ['$scope', 'dogeData', function($scope, dogeData) {
   
   $scope.multiplier = 1;
   
@@ -10,11 +10,11 @@ angular.module('wowSuch').controller('dogeController', function($scope, dogeData
     var doge = data.return.markets.DOGE;
     $scope.value = doge.lasttradeprice;
   });
-});
+}]);
 
-angular.module('wowSuch').factory('dogeData', function($http) {
+angular.module('wowSuch').factory('dogeData', ['$http', function($http) {
   var dogeUrl = 'http://jsonp.jit.su/?callback=soCallback&url=http%3A%2F%2Fpubapi.cryptsy.com%2Fapi.php%3Fmethod%3Dsinglemarketdata%26marketid%3D132&callback=JSON_CALLBACK';
   return $http.jsonp(dogeUrl);
-});
+}]);
 
 function soCallback(data) {}
