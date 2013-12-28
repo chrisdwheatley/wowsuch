15845964ced0461c0d6b202a7a5cda0aab53bcd7
Chris Wheatley <swirlycheetah@users.noreply.github.com>
2013-12-28 17:45:12 -0500

    donations data retrieval

--- src/such.djs
+++ src/such.djs
@@ -5,11 +5,14 @@ trained
 plz angular.module with 'wowSuch', []
 
 plz angular.module with 'wowSuch'&
-  dose controller with 'dogeController', ['$scope', 'dogeData' much $scope dogeData
+  dose controller with 'dogeController', ['$scope', 'dogeData', 'donationData', much $scope dogeData donationData
   $scope.multiplier is 1;
   dogeData dose success with much data
     very doge is data.return.markets.DOGE
     $scope.value is doge.lasttradeprice;
+  wow&
+  donationData dose success with much data
+    $scope.donations = data.donations;
   wow&
 ;
 wow
@@ -19,6 +22,11 @@ plz angular.module with 'wowSuch'&
   dose factory with 'dogeData', ['$http' much $http
     very dogeUrl is 'https://jsonp.nodejitsu.com/?callback=soCallback&url=http%3A%2F%2Fpubapi.cryptsy.com%2Fapi.php%3Fmethod%3Dsinglemarketdata%26marketid%3D132&callback=JSON_CALLBACK'
     wow $http.jsonp(dogeUrl)
+]);
+
+plz angular.module with 'wowSuch'&
+  dose factory with 'donationData', ['$http', much $http
+  wow $http.get('donations.json');
 ]);
 
 such soCallback much data
--- src/such.js
+++ src/such.js
@@ -5,13 +5,16 @@
 angular.module('wowSuch', []);
 
 angular.module('wowSuch')
-    .controller('dogeController', ['$scope', 'dogeData',
-        function($scope, dogeData) {
+    .controller('dogeController', ['$scope', 'dogeData', 'donationData',
+        function($scope, dogeData, donationData) {
             $scope.multiplier = 1;
             dogeData.success(function(data) {
                 var doge = data.
                 return .markets.DOGE;
                 $scope.value = doge.lasttradeprice;
+            })
+            donationData.success(function(data) {
+                $scope.donations = data.donations;
             });
         }
     ]);
@@ -21,6 +24,13 @@ angular.module('wowSuch')
         function($http) {
             var dogeUrl = 'https://jsonp.nodejitsu.com/?callback=soCallback&url=http%3A%2F%2Fpubapi.cryptsy.com%2Fapi.php%3Fmethod%3Dsinglemarketdata%26marketid%3D132&callback=JSON_CALLBACK';
             return $http.jsonp(dogeUrl);
+        }
+    ]);
+
+angular.module('wowSuch')
+    .factory('donationData', ['$http',
+        function($http) {
+            return $http.get('donations.json');;
         }
     ]);
 
