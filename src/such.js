a35230c2a20cadb85a13f6692a188d45cf4b6592
Chris Wheatley <swirlycheetah@users.noreply.github.com>
2014-01-02 15:08:57 -0500

    amending dat retreival url

--- src/such.js
+++ src/such.js
@@ -1,4 +1,4 @@
-// such.js is generated from such.djs
+// such.js is generated from such.djs 
 
 "use strict";
 
@@ -48,7 +48,7 @@ angular.module('wowSuch')
 angular.module('wowSuch')
     .factory('dogeData', ['$http',
         function($http) {
-            var dogeUrl = 'https://jsonp.nodejitsu.com/?callback=soCallback&url=http%3A%2F%2Fpubapi.cryptsy.com%2Fapi.php%3Fmethod%3Dsinglemarketdata%26marketid%3D132&callback=JSON_CALLBACK';
+            var dogeUrl = 'https://lit-beach-8985.herokuapp.com/?callback=soCallback&url=http%3A%2F%2Fpubapi.cryptsy.com%2Fapi.php%3Fmethod%3Dsinglemarketdata%26marketid%3D132&callback=JSON_CALLBACK';
             return $http.jsonp(dogeUrl);
         }
     ]);
