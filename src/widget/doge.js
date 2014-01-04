27ac93b3bb0021db0723126765658bc04571cf5b
Chris Wheatley <swirlycheetah@users.noreply.github.com>
2014-01-03 19:06:15 -0500

    upping version of jquery

--- src/widget/doge.js
+++ src/widget/doge.js
@@ -3,11 +3,11 @@
   var jQuery;
 
   /******** Load jQuery if not present *********/
-  if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.4.2') {
+  if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.10.1') {
       var script_tag = document.createElement('script');
       script_tag.setAttribute("type","text/javascript");
       script_tag.setAttribute("src",
-          "https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
+          "https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js");
       if (script_tag.readyState) {
         script_tag.onreadystatechange = function () { // For old versions of IE
             if (this.readyState == 'complete' || this.readyState == 'loaded') {
@@ -59,6 +59,5 @@
           setInterval(function(){ poll(); }, 5000);
       });
   }
-
 
 }) ();
