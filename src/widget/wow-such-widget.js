8752506c9b5b084483dbb30d5099b3d3238a6efa
7THStage <idigdoge@gmail.com>
2014-01-29 11:30:14 -0800

    Good old-fashioned JS
    Tested in the latest versions of Firefox, Chrome, Opera, and Safari on
    Mac 10.9. Tested in IE10 on Windows 7.

--- src/widget/wow-such-widget.js
+++ src/widget/wow-such-widget.js
@@ -1,77 +1,172 @@
 (function() {
-  var jQuery;
+  // If people have multiple widgets on a page, including script tags, only one of these things is requried to update them all
+  if (window.wowSuchInit) return;
+  window.wowSuchInit = true;
+  
   var previousValue = null;
-
-  // load jQuery if it hasn't already been loaded in
-  // @TODO - remove dependency on jQuery
-  if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.10.1') {
-    var script_tag = document.createElement('script');
-    script_tag.setAttribute("type","text/javascript");
-    script_tag.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js");
-
-    if (script_tag.readyState) {
-      script_tag.onreadystatechange = function () { // For old versions of IE
-        if (this.readyState == 'complete' || this.readyState == 'loaded') {
-          scriptLoadHandler();
-        }
-      };
-    } else { // Other browsers
-      script_tag.onload = scriptLoadHandler;
-    }
-    // Try to find the head, otherwise default to the documentElement
-    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
-  } else {
-    // The jQuery version on the window is the one we want to use
-    jQuery = window.jQuery;
-    main();
-  };
-
-  // called once jQuery loaded
-  function scriptLoadHandler() {
-    // Restore $ and window.jQuery to their previous values and store the
-    // new jQuery in our local jQuery variable
-    jQuery = window.jQuery.noConflict(true);
-    // Call our main function
-    main();
-  }
-
-  // main function
-  function main() {
-    jQuery(document).ready(function($) {
-      // load css
-      var css_link = $("<link>", {
-        rel: "stylesheet",
-        type: "text/css",
-        href: "widget/wow-such-widget.css"
-      });
-      css_link.appendTo('head');
-
-      // widget boilerplate until data loads
-      $(".wow-such-widget").html('<div class="wow-such-widget-overlay"></div><div class="wow-such-widget-lower-bar"></div>');
-
-      var dogeApiUrl = 'https://lit-beach-8985.herokuapp.com/?callback=?&url=https://www.coins-e.com/api/v2/market/DOGE_BTC/depth';
-
-      // poll api for data and display contents of widget on retrieval, flash on price change
-      function poll(){
-        $.getJSON(dogeApiUrl, function(data){
-          var data = data.ltp;
-          $('.wow-such-widget').html('<div class="wow-such-widget-inner">1 DOGE = <span class="wow-such-widget-inner-price"></span> BTC</div><div class="wow-such-widget-credit">Powered by <a href="http://wowsuch.io/?utm_source='+ document.URL +'&utm_medium=widget&utm_campaign=wowsuchwidget" target="_blank">wowsuch.io</a></div><div class="wow-such-widget-lower-bar"></div>');
-
-            if(data !== previousValue && previousValue) {
-              $(".wow-such-widget-inner-price").addClass('wow-such-widget-animate');
-              setTimeout(function(){
-                $(".wow-such-widget-inner-price").removeClass('wow-such-widget-animate');
-              }, 2000);
-            };
-
-          $(".wow-such-widget-inner-price").html(data);
-          previousValue = data;
-        });
+  var dogeApiUrl = 'https://lit-beach-8985.herokuapp.com/?url=https://www.coins-e.com/api/v2/market/DOGE_BTC/depth';
+  
+  function getJSONP(url, callback) {
+    var c = 'gjsonp' + Date.now().toString().substr(-4) + Math.random().toString().substr(2, 6);
+    
+    // URL already has a query string
+    if (url.indexOf('?') >= 0) {
+      // Already has the callback parameter
+      if (url.indexOf('callback=') > 0) {
+        url = url.replace(/callback=[^&]*/, 'callback=' + c);
+      } else {
+        url += ('&callback=' + c);
       }
-
-      // call poll function initially and periodically
-      poll();
-      setInterval(poll, 10000);
+    } else {
+      url += ('?callback=' + c);
+    }
+    
+    var scriptTag = document.createElement('script');
+    scriptTag.async = 1;
+    scriptTag.src = url;
+    
+    window[c] = function(data) {
+      // Call it
+      callback(data);
+      
+      // Clean up the script tag
+      scriptTag.parentNode.removeChild(scriptTag);
+      
+      // Delete this callback
+      delete window[c];
+    };
+    
+    var firstScript = document.getElementsByTagName('script')[0];
+    firstScript.parentNode.insertBefore(scriptTag, firstScript);
+  };
+  
+  function regExpEscape(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  };
+  
+  function SearchNodeList() {
+    this.nodes = [];
+    this.quick = (typeof this.nodes.forEach === 'function');
+  };
+  
+  SearchNodeList.prototype.forEach = function(f) {
+    if (this.quick) {
+      this.nodes.forEach(f);
+    } else {
+      for (var i = 0, x = this.nodes.length; i < x; i += 1) {
+        f(this.nodes[i], i);
+      }
+    }
+    
+    return this;
+  };
+  
+  SearchNodeList.prototype.push = function(node) {
+    this.nodes.push(node);
+    
+    return this;
+  };
+  
+  SearchNodeList.prototype.html = function(html) {
+    this.forEach(function(e) {
+      e.innerHTML = html;
     });
-  }
+    
+    return this;
+  };
+  
+  SearchNodeList.prototype.addClass = function(c) {
+    this.forEach(function(e) {
+      // Make sure we're not adding the same class twice
+      if (e.className.indexOf(c) < 0) {
+        // Add a space before it if it's not the first
+        if (e.className && e.className.length > 0) {
+          c = (' ' + c);
+        }
+        
+        e.className += c;
+      }
+    });
+    
+    return this;
+  };
+  
+  SearchNodeList.prototype.removeClass = function(c) {
+    // Make sure we get rid of any spaces
+    var reg = new RegExp(regExpEscape(c));
+    var clean = /\s{2,}/g;
+    
+    this.forEach(function(e) {
+      if (e.className && e.className.length > 0) {
+        e.className = e.className.replace(reg, '').replace(clean, ' ').trim();
+      }
+    });
+    
+    return this;
+  };
+  
+  function searchNodes(array, c) {
+    var r = new SearchNodeList();
+    
+    var reg = new RegExp(regExpEscape(c));
+    for (var i = 0, x = array.length; i < x; i += 1) {
+      if (reg.test(array[i].className)) r.push(array[i]);
+    }
+    
+    return r;
+  };
+  
+  var oldOnLoad = window.onload;
+  
+  function main() {
+    // Load the CSS
+    var cssLink = document.createElement('link');
+    cssLink.setAttribute('rel', 'stylesheet');
+    cssLink.setAttribute('type', 'text/css');
+    cssLink.setAttribute('href', 'widget/wow-such-widget.css');
+    
+    (document.getElementsByTagName('head')[0] || document.documentElement).appendChild(cssLink);
+    
+    // Build the widget HTML
+    var divs = document.getElementsByTagName('div');
+    var spans = document.getElementsByTagName('span');
+    
+    searchNodes(divs, 'wow-such-widget').html('<div class="wow-such-widget-overlay"></div><div class="wow-such-widget-lower-bar"></div>');
+    
+    var fullHTML = false;
+    
+    function poll() {
+      getJSONP(dogeApiUrl, function(data) {
+        var ltp = data.ltp;
+        
+        // Only do this on the first load
+        if (!fullHTML) {
+          searchNodes(divs, 'wow-such-widget').html('<div class="wow-such-widget-inner">1 DOGE = <span class="wow-such-widget-inner-price"></span> BTC</div><div class="wow-such-widget-credit">Powered by <a href="http://wowsuch.io/?utm_source=' + document.URL + '&utm_medium=widget&utm_campaign=wowsuchwidget" target="_blank">wowsuch.io</a></div><div class="wow-such-widget-lower-bar"></div>');
+          fullHTML = true;
+        }
+        
+        // Animate if the price has changed
+        var prices = searchNodes(spans, 'wow-such-widget-inner-price');
+        if (ltp !== previousValue && previousValue) {
+          prices.addClass('wow-such-widget-animate');
+          setTimeout(function() {
+            prices.removeClass('wow-such-widget-animate');
+          }, 2000);
+        }
+        
+        // Fill in the new value
+        prices.html(ltp);
+        previousValue = ltp;
+      });
+    }
+    
+    // Call poll function initially and periodically
+    poll();
+    setInterval(poll, 10000);
+    
+    // Do the other window.onload stuff
+    if (oldOnLoad) oldOnLoad.apply(this, arguments);
+  };
+  
+  window.onload = main;
 })();
